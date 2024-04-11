8bcba03071830ae10eb2e8ffd07434202ed647ff
pardeep kaur <137564227+Pardeep32@users.noreply.github.com>
2024-04-11 14:33:55 -0400

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "redditbucketclone1708" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
-    region = "ap-southeast-2"
+    region = "ca-central-1"
   }
 }
